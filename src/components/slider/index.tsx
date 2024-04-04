fdc5d56aef4540714538dee615d8d52de3e18429
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-04-04 12:37:38 -0300

    slider pronto

--- src/components/slider/index.tsx
+++ src/components/slider/index.tsx
@@ -4,11 +4,11 @@ import Image from "next/image"
 
 export default function Slider() {
     const mediaList = [
-        { id: 1, type: 'image', src: '/banner/banner.png', alt: 'Banner', width: "100%", height: "100%" },
-        { id: 2, type: 'image', src: '/banner/banner2.png', alt: 'Banner2' },
-        { id: 3, type: 'image', src: '/banner/banner3.jpg', alt: 'Banner3' },
-        { id: 4, type: 'image', src: '/banner/banner4.jpg', alt: 'Banner4' },
-        { id: 5, type: 'image', src: '/banner/banner5.jpg', alt: 'Banner5' },
+        { id: 1, type: 'image', src: '/banner/banner.png', alt: 'Banner', idInput: "slide1 s1"},
+        { id: 2, type: 'image', src: '/banner/banner2.png', alt: 'Banner2', idInput: "slide2" },
+        { id: 3, type: 'image', src: '/banner/banner3.jpg', alt: 'Banner3', idInput: "slide3" },
+        { id: 4, type: 'image', src: '/banner/banner4.jpg', alt: 'Banner4', idInput: "slide4" },
+        { id: 5, type: 'image', src: '/banner/banner5.jpg', alt: 'Banner5', idInput: "slide5" },
     ]
     const [contador, setContador] = useState(1)
 
@@ -23,16 +23,15 @@ export default function Slider() {
         <div className="Container">
             {mediaList.map((imagem, index) =>
                 <div key={imagem.id} style={{ display: index === contador ? 'block' : 'none' }}>
-                    <input type="radio" name="slide" id="slide1" checked />
-                    <Image src={imagem.src} alt={imagem.alt} sizes="100%" fill />
+                    <Image src={imagem.src} alt={imagem.alt} sizes="100%" fill className="slides"/>
                 </div>
             )}
             <div className="navigation">
-                <label className="bar"></label>
-                <label className="bar"></label>
-                <label className="bar"></label>
-                <label className="bar"></label>
-                <label className="bar"></label>
+                <div className="bar" id="slide0" onClick={(e) => setContador(0)} style={{ backgroundColor: contador === 0 ? 'white' : 'transparent'}}></div>
+                <div className="bar" id="slide1" onClick={(e) => setContador(1)} style={{ backgroundColor: contador === 1 ? 'white' : 'transparent'}}></div>
+                <div className="bar" id="slide2" onClick={(e) => setContador(2)} style={{ backgroundColor: contador === 2 ? 'white' : 'transparent'}}></div>
+                <div className="bar" id="slide3" onClick={(e) => setContador(3)} style={{ backgroundColor: contador === 3 ? 'white' : 'transparent'}}></div>
+                <div className="bar" id="slide4" onClick={(e) => setContador(4)} style={{ backgroundColor: contador === 4 ? 'white' : 'transparent'}}></div>
             </div>
         </div>
     )
