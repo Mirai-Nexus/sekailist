7843c2cab356173382f8d8d7bf16bb38efc5f5fb
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-04-04 10:35:23 -0300

    aprimoramento do carrousel resta passar para  a direita e passar ao clicar no label

--- src/components/slider/index.tsx
+++ src/components/slider/index.tsx
@@ -1,27 +1,39 @@
 import "./style.css"
-import { useState,useEffect } from "react"
-
+import { useState, useEffect } from "react"
+import Image from "next/image"
 
 export default function Slider() {
     const mediaList = [
-        {id: 1, type: 'image', src: '/banner/banner.png', alt: 'Banner'},
-        {id: 2, type: 'image', src: '/banner/banner2.png', alt: 'Banner2'},
-        {id: 3, type: 'image', src: '/banner/banner3.jpg', alt: 'Banner3'},
-        {id: 4, type: 'image', src: '/banner/banner4.jpg', alt: 'Banner4'},
-        {id: 5, type: 'image', src: '/banner/banner5.jpg', alt: 'Banner5'},
+        { id: 1, type: 'image', src: '/banner/banner.png', alt: 'Banner', width: "100%", height: "100%" },
+        { id: 2, type: 'image', src: '/banner/banner2.png', alt: 'Banner2' },
+        { id: 3, type: 'image', src: '/banner/banner3.jpg', alt: 'Banner3' },
+        { id: 4, type: 'image', src: '/banner/banner4.jpg', alt: 'Banner4' },
+        { id: 5, type: 'image', src: '/banner/banner5.jpg', alt: 'Banner5' },
     ]
-    const[contador,setContador] = useState(1)
+    const [contador, setContador] = useState(1)
 
     useEffect(() => {
-        const timer = setInterval(() =>{
+        const timer = setInterval(() => {
             setContador((contador) => (contador + 1) % mediaList.length);
-        },5000);
+        }, 5000);
         return () => clearInterval(timer);
     }, [contador, mediaList.length]);
 
-    return(
+    return (
         <div className="Container">
-            <div className="Banner" style={{backgroundImage: `url(${mediaList[contador].src})`}} />
+            {mediaList.map((imagem, index) =>
+                <div key={imagem.id} style={{ display: index === contador ? 'block' : 'none' }}>
+                    <input type="radio" name="slide" id="slide1" checked />
+                    <Image src={imagem.src} alt={imagem.alt} sizes="100%" fill />
+                </div>
+            )}
+            <div className="navigation">
+                <label className="bar"></label>
+                <label className="bar"></label>
+                <label className="bar"></label>
+                <label className="bar"></label>
+                <label className="bar"></label>
+            </div>
         </div>
     )
 }
