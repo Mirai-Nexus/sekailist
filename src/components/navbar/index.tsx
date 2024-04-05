da0cc3f842118ecf3f1f7e8d9a6d20faf6045819
Heric Virgilio <heric.v.v.santos@gmail.com>
2024-04-05 12:12:40 -0300

    iniciando responsividade

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -5,21 +5,41 @@ import Search from "../search"
 
 export default function NavBar() {
     const [inputValue, setInputValue] = useState("")
+    const [Menu, setMenu] = useState('none')
+
+    const clickMenu = () => {
+        if(Menu === 'none'){
+            setMenu('block')
+        }else{
+            setMenu('none')
+        }
+    }
+
     const handleChange = (evento: { target: { value: SetStateAction<string> } }) => {
         setInputValue(evento.target.value)
     }
     return (
         <div className="ContainerNavBar" style={{ color: "white" }}>
-                <h1 className="Logo">SL</h1>
-                <Search/>
-                <ul className="ContainerLista">
+            <div className="ContainerMenuHamburguer" onClick={clickMenu}>
+                <Image src={"/icons/hamburguer.svg"} alt="menu hamburguer" width={40} height={40} />
+            </div>
+            <h1 className="Logo">SL</h1>
+            <Search />
+            <ul className="ContainerLista">
+                <li>Animes</li>
+                <li>Mangás</li>
+                <li>Notícias</li>
+            </ul>
+            <div className="ContainerProfile">
+                <Image src={'/icons/profile.svg'} alt="oi" width={35} height={35} />
+            </div>
+            <div className="MenuEscondido" style={{display: Menu}}>
+                <ul className="ContainerListaMob" style={{ display: 'block' }}>
                     <li>Animes</li>
                     <li>Mangás</li>
                     <li>Notícias</li>
                 </ul>
-                <div className="ContainerProfile">
-                    <Image src={'/icons/profile.svg'} alt="oi" width={35} height={35} />
-                </div>
+            </div>
         </div>
     )
 }
